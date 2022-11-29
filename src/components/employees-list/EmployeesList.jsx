1e8884d7748bf25d36528033088dfe004174d643
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-11-29 08:03:21 +0000

    create components

---
+++ src/components/employees-list/EmployeesList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EmployeesList() {
+  return (
+    <div>EmployeesList</div>
+  )
+}
---
+++ src/components/search-panel/SearchPanel.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SearchPanel() {
+  return (
+    <div>SearchPanel</div>
+  )
+}
